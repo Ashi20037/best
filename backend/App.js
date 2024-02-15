f6784542f647ae96abf34cdc9253eb48423f6def
ashikakj2003@gmail.com <ashikakj2003@gmail.com>
2024-02-15 23:04:47 +0530

    commit

---
+++ backend/App.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+const blogRoutes = require('./routes/blogRoutes');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+app.use('/auth', authRoutes);
+app.use('/blogs', blogRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
